f27ce71446e4add7b63c3cd35e57e22e58c92a7d
ymonb1291 <ymonb1291@gmail.com>
2020-10-10 21:22:56 +0200

    Bug fix

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 import { Level } from "./level.enum.ts";
 
-export const DEFAULT_LEVEL: Level = Level.log;
+export const DEFAULT_LEVEL: Level = Level.info;
 export const DEFAULT_TIME: boolean = true;
