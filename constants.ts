dae579e9d71320311bb5c5c51a74a9ccc388821f
ymonb1291 <ymonb1291@gmail.com>
2020-10-11 22:30:48 +0200

    Update default values

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
 import { Level } from "./level.enum.ts";
 
 export const DEFAULT_JSON: boolean = true;
-export const DEFAULT_LEVEL: Level = Level.info;
+export const DEFAULT_LEVEL: Level = Level.trace;
 export const DEFAULT_MERGE_BINDINGS: boolean = true;
 export const DEFAULT_MERGE_META: boolean = true;
 export const DEFAULT_TIME: boolean = true;
-export const DEFAULT_USE_LABELS: boolean = true;
+export const DEFAULT_USE_LABELS: boolean = false;
 export const LOG_VERSION: number = 1;
