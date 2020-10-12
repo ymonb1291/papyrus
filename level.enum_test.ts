484832b5a54d3b0c142da43c10d50e702bd23f1b
ymonb1291 <ymonb1291@gmail.com>
2020-10-12 19:15:15 +0200

    Add tests of level.enum.ts

---
+++ level.enum_test.ts
@@ -0,0 +1,42 @@
+import { Rhum } from "./deps_test.ts";
+import { Level } from "./level.enum.ts";
+import { Papyrus } from "./papyrus.ts";
+
+Rhum.testPlan("level.enum.ts", () => {
+
+  Rhum.testSuite("Level", () => {
+    const numbers: string[] = [];
+    const strings: string[] = [];
+
+    Rhum.testCase("Enum values are numbers", () => {
+
+      Object
+        .keys(Level)
+        .forEach(key => {
+          if(isNaN(parseInt(key))) {
+            strings.push(key);
+          } else {
+            numbers.push(key);
+          }
+        });
+        
+      Rhum.asserts.assertEquals(numbers.length, strings.length)
+    });
+
+    Rhum.testCase("Enum keys correspond to a method of Papyrus", () => {
+      const logger = new Papyrus;
+      
+      const res = strings
+        .map(key => {
+          return !!logger[key as keyof Papyrus];
+        })
+        .every(el => el);
+
+      Rhum.asserts.assert(res);
+    });
+
+  });
+
+});
+
+Rhum.run();
