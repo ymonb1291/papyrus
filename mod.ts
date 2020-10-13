6279595c84359f532f32ec988302c120c7910b22
ymonb1291 <ymonb1291@gmail.com>
2020-10-13 23:19:11 +0200

    Export additional types

--- mod.ts
+++ mod.ts
@@ -5,4 +5,8 @@ export default Papyrus;
 // Named exports
 export { Level } from "./level.enum.ts";
 export { Papyrus } from "./papyrus.ts";
+
+// Type exports
+export type { ChildOptions } from "./childoptions.interface.ts";
+export type { Log, LogWithError, LogWithMessage } from "./log.interface.ts";
 export type { PapyrusOptions } from "./papyrusoptions.interface.ts";
