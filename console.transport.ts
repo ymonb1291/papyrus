de33c9c5082a7e3f59026a9eaee540caa9430493
ymonb1291 <ymonb1291@gmail.com>
2020-10-17 14:28:47 +0200

    Remove unnecessary import

--- console.transport.ts
+++ console.transport.ts
@@ -1,5 +1,4 @@
 import type { Transport } from "./transport.interface.ts";
-import type { Log } from "./log.interface.ts";
 
 export class PapyrusConsole implements Transport {
   public log(data: string) {
